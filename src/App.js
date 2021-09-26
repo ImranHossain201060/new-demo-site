74c5d21e69f6b4172e969cdc8878a4e1e5509e3e
ImranHossain201060 <imran201060@gmail.com>
2021-09-26 15:58:28 +0600

    first added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Data from './components/Data/Data';
+import Header from './components/Header/Header';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Data />
+    </div>
+  );
+}
+
+export default App;
